d0023bb37c9fc361b1fda225b8e81b1143dcd0e6
KirillDanProg <kirill2898v@gmail.com>
2023-01-07 23:05:49 +0300

    test

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ app.use(cors())
 app.use(express.json())
 
 const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     service: 'gmail',
     auth: {
         user: process.env.EMAIL,
