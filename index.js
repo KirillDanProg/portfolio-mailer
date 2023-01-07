a17168990dccdd049bd865a76f1096e05672b57a
KirillDanProg <kirill2898v@gmail.com>
2023-01-07 21:39:19 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
+import nodemailer from "nodemailer"
+import * as Process from "process";
 
-const PORT = process.env.PORT || 4444
+const PORT = 4444
 
 dotenv.config()
 
@@ -11,11 +13,39 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.post("/send-email", (res, req) => {
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: Process.env.EMAIL,
+        pass: Process.env.APP_PASSWORD
+    }
+});
 
+app.get("/", (req, res) => {
+    res.send("Hello world")
+})
+
+app.post("/send-email", async (req, res) => {
+
+    const mailOptions = {
+        from: Process.env.EMAIL,
+        to: Process.env.EMAIL,
+        subject: 'portfolio mail',
+        text: '',
+        html: `<h1>Email test</h1>`
+    };
+
+    await transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+            // do something useful
+        }
+    });
 })
 
 app.listen(PORT, () => {
-    console.log("server has been started on port ")
+    console.log("server has been started on port " + PORT)
 })
 
